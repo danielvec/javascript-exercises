53fc35424c34e16bf39b0efd12970fe98ff8293b
DanielVec <vechioladaniel@gmail.com>
2022-02-10 20:17:41 -0800

    finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a, b) => a + b, 0);
+}
+
+
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b);
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	total = 1;
+  for (i = 1; i <= a ; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
